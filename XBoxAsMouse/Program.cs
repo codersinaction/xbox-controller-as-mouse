67a071e0044cfeffa3d65dac35851569270800e6
Fotis Karagiorgos <fotiskaragiorgos@gmail.com>
2018-10-22 23:02:23 +0300

    Created project

--- XBoxAsMouse/Program.cs
+++ XBoxAsMouse/Program.cs
@@ -1,15 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XBoxAsMouse
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
+			var inputMonitor = new ControllerToMouseProxy();
+			inputMonitor.Start();
+			Console.ReadLine();
 		}
 	}
 }
